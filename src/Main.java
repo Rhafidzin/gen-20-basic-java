b3f063c1e8e88a5f2988d1b61fe25ed63654d90d
Rahman Hafidzin <rhafidzin@gmail.com>
2024-02-27 20:42:26 +0700

    Mengeluarkan output string 'Hello World!'

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+
+
+
+//membuat class public bernama Main yang harus sesuai dengan nama file
+public class Main {
+    //membuat main method untuk mengeksekusi program
+    public static void main(String[] args) {
+        //Membuat variable string berisi Hello World!
+        String output = "Hello World!";
+
+        //Mengeluarkan hasil output "Hello world!" di terminal
+        System.out.println(output);
+    }
+}
